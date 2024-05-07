d30dc2e7bdf849d437343c4f3779fe8df7e49cea
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-05-07 18:06:54 -0300

    login styles

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,16 +1,39 @@
 export default function Login() {
   return (
-    <main>
-      <form action="">
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="text" />
+    <main className="login_container text-white flex flex-col justify-center">
+      <div className="flex flex-col items-center text-center mb-10">
+        <h1 class="text-4xl font-bold uppercase">Tomás Sorgetti</h1>
+        <span class="text-base font-medium uppercase">Dashboard</span>
+      </div>
+      <form
+        action=""
+        className="w-[477px] h-[549px] bg-glassColor border-[1px] rounded-[28px] mx-auto flex flex-col items-center p-20 justify-between"
+      >
+        <div className="flex flex-col gap-6 w-full">
+          <span className="uppercase font-bold shadow-shadow text-center">
+            login
+          </span>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="font-semibold">
+              Email:
+            </label>
+            <input type="text" className="h-10 rounded" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="" className="font-semibold">
+              Password:
+            </label>
+            <input type="password" className="h-10 rounded" />
+          </div>
+          <div className="flex gap-2 items-center">
+            <input type="checkbox" name="" id="" />
+            <label htmlFor="">Remember me</label>
+          </div>
         </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <input type="password" />
-        </div>
-        <button>Login</button>
+
+        <button className="font-bold uppercase py-3 px-20 bg-gradient-to-r from-pinkColor2 to-pinkColor rounded">
+          Login
+        </button>
       </form>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        bgColor: "#050310",
+        textColor: "#D1D1D1",
+        pinkColor: "#FE0072",
+        pinkColor2: "#E80ECD",
+        footerColor: "#ffffff54",
+        glassColor: "rgba(255, 255, 255, 0.2)",
+        shadow: "rgba(132, 57, 249, 1)",
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.125rem",
+        "2xl": "1.5rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+        subtitles: "1.6rem",
+        titles: "2rem",
+        titlesMobile: "1.5rem",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
