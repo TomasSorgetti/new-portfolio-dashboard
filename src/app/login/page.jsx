3d399fdbbdf22fce0bb49283b579fb8fb497c0a4
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-05-07 16:12:11 -0300

    first commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+  return (
+    <main>
+      <form action="">
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input type="password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </main>
+  );
+}
