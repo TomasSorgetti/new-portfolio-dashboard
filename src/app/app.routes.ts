28c36e198bb10b7d855f2d93617ec9e65e381e91
TomasSorgetti <tomassorgetti456@gmail.com>
2024-08-06 17:50:00 -0300

    get about data && render

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'about', component: AboutComponent },
 ];
---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { AboutService } from '../../services/about.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+  location:string = "";
+  description:string = "";
+  description2:string = "";
+  sistem:string = "";
+  ide:string = "";
+  hobby:string = "";
+  music: string = "";
+  
+  public AboutService = inject(AboutService)
+  private router = inject(Router);
+
+  constructor() {
+    this.AboutService.getAbout().subscribe({
+      next: (data) => {
+        if (!data.error) {
+        this.location = data.about.location;
+        this.description = data.about.description;
+        this.description2 = data.about.description2;
+        this.sistem = data.about.sistem;
+        this.ide = data.about.ide;
+        this.hobby = data.about.hobby;
+        this.music = data.about.music;
+        }
+      },
+      error: (err) => {
+        console.log(err.error);
+      }
+    })
+  }
+}
+
