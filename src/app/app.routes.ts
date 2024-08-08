92ce29953a28a58bbb07f376d853a311a8fe5622
TomasSorgetti <tomassorgetti456@gmail.com>
2024-08-07 22:35:13 -0300

    add new project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AboutUpdateComponent } from './pages/about-update/about-update.component';
+import { AddNewProjectComponent } from './pages/add-new-project/add-new-project.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'about', component: AboutComponent },
     { path: 'about-update', component: AboutUpdateComponent },
+    { path: 'add-new-project', component: AddNewProjectComponent },
     
     { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
